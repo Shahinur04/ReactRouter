1018426f0221b6e5768cddee9844f85723564c66
HmSani6699 <shahinur04042002@gmail.com>
2023-04-07 00:22:28 +0600

    postdetail

---
+++ src/Components/PostDetail/PostDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostDetail = () => {
+    return (
+        <div>
+            <h2>Show your all post details</h2>
+        </div>
+    );
+};
+
+export default PostDetail;
--- src/Components/pst/Pst.jsx
+++ src/Components/pst/Pst.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './pst.css'
 
 const Pst = ({pst}) => {
     const{id,title,body}=pst;
     return (
-        <div>
+        <div className='pst'>
             <p>id:{id}</p>
             <h2>Title{title}</h2>
-            <h3>body:{body}</h3>
+            {/* <h3>body:{body}</h3> */}
+            <Link to={`/pst/${id}`}>pst</Link>
+            <button>Show post detail</button>
         </div>
     );
 };
